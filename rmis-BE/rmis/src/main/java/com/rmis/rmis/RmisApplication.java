3905de7d58847e4de55b580aba2443a57f630db1
Thenul-Ranmuthu <thenul.ranmuthu@gmail.com>
2026-02-19 16:49:28 +0530

    added new files

---
+++ rmis-BE/rmis/src/main/java/com/rmis/rmis/RmisApplication.java
@@ -0,0 +1,13 @@
+package com.rmis.rmis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmisApplication.class, args);
+	}
+
+}
---
+++ rmis-BE/rmis/src/test/java/com/rmis/rmis/RmisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmis.rmis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
