722d1e208fdc76a2bbcd389085f00587a20c97a5
luuha2004 <praviniluhansa@gmail.com>
2026-03-18 14:46:35 +0530

    Remove .next from tracking

--- rmis-FE/.next/dev/server/chunks/ssr/_next-internal_server_app_company_dashboard_page_actions_e2d22a51.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/company/dashboard/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_company_dashboard_page_actions_e2d22a51.js.map
