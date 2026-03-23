8b179fc255bdeb2dd41a82370fe47c3cb8a102ad
luuha2004 <praviniluhansa@gmail.com>
2026-03-23 18:10:13 +0530

    Modified an import statement

--- rmis-BE/rmis/src/main/java/com/rmis/rmis/services/impl/QuotaRequestServiceImpl.java
+++ rmis-BE/rmis/src/main/java/com/rmis/rmis/services/impl/QuotaRequestServiceImpl.java
@@ -2,12 +2,12 @@ package com.rmis.rmis.services.impl;
 
 import com.rmis.rmis.domain.dtos.PagedResponseDto;
 import com.rmis.rmis.domain.dtos.QuotaRequestDetailDto;
+import com.rmis.rmis.domain.dtos.QuotaRequestHeaderDto;
 import com.rmis.rmis.domain.dtos.QuotaRequestResponseDto;
 import com.rmis.rmis.domain.entities.Company;
 import com.rmis.rmis.domain.entities.QuotaRequest;
 import com.rmis.rmis.enums.QuotaRequestStatus;
 import com.rmis.rmis.repositories.CompanyRepository;
-import com.rmis.rmis.domain.enums.QuotaRequestStatus;
 import com.rmis.rmis.exceptions.QuotaRequestNotFoundException;
 import com.rmis.rmis.repositories.QuotaRequestRepository;
 import com.rmis.rmis.services.QuotaRequestsSpecification;
@@ -106,8 +106,6 @@ public class QuotaRequestServiceImpl implements QuotaRequestService {
 
     private QuotaRequestResponseDto toDto(QuotaRequest entity) {
         return QuotaRequestResponseDto.builder()
-                // .requestId(formatRequestId(entity.getRequestNumber()))
-                // .requestId(entity.getRequestId().toString())
                 .id(entity.getRequestId())
                 .requestId(formatRequestId(entity.getRequestNumber()))
                 .companyName(entity.getCompanyName())
@@ -117,9 +115,9 @@ public class QuotaRequestServiceImpl implements QuotaRequestService {
                 .build();
     }
 
-    // private String formatRequestId(Long requestNumber) {
-    //     return String.format("REQ-%04d", requestNumber);
-    // }
+     private String formatRequestId(Long requestNumber) {
+         return String.format("REQ-%04d", requestNumber);
+     }
 
     @Override
     public String addQuotaRequest(QuotaRequestHeaderDto quotaRequestHeaderDto) {
