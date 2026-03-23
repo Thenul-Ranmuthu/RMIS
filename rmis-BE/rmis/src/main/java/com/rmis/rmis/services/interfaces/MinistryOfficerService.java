614dcc02799501f43f583e6d5d3f9d8a5bffe03d
luuha2004 <praviniluhansa@gmail.com>
2026-03-23 18:04:39 +0530

    RMIS-27 Add MinistryOfficer as a parameter to Request approval and rejection methods and call logging methods inside Request approval and rejection methods

--- rmis-BE/rmis/src/main/java/com/rmis/rmis/services/interfaces/MinistryOfficerService.java
+++ rmis-BE/rmis/src/main/java/com/rmis/rmis/services/interfaces/MinistryOfficerService.java
@@ -1,11 +1,13 @@
 package com.rmis.rmis.services.interfaces;
 
+import com.rmis.rmis.domain.entities.MinistryOfficer;
+
 import java.util.UUID;
 
 public interface MinistryOfficerService {
 
-    String changeQuotaRequestStatusApprove(UUID id);
+    String changeQuotaRequestStatusApprove(UUID id, MinistryOfficer officer);
 
-    String changeQuotaRequestStatusReject(UUID id);
+    String changeQuotaRequestStatusReject(UUID id, MinistryOfficer officer);
 
 }
