d2869117f4e015836b618dff1cf1375dccc8cc56
luuha2004 <praviniluhansa@gmail.com>
2026-04-16 23:59:45 +0530

    RMIS-45 modify: add attribute approvedAmount

--- rmis-BE/rmis/src/main/java/com/rmis/rmis/domain/entities/QuotaRequest.java
+++ rmis-BE/rmis/src/main/java/com/rmis/rmis/domain/entities/QuotaRequest.java
@@ -55,6 +55,9 @@ public class QuotaRequest {
     @Column(name = "requested_quota", nullable = false, precision = 15, scale = 2)
     private BigDecimal requestedQuota;
 
+    @Column(name = "approved_amount")
+    private BigDecimal approvedAmount;  // null until approved
+
     @Column(name = "submission_date", nullable = false, updatable = false)
     private LocalDateTime submissionDate;
 
